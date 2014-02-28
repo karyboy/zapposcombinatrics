9dcc914832675ebc9cf00424394ab43c413d8aa9
Karnesh Mehra <karneshmehra@gmail.com>
2014-02-28 05:33:50 -0500

    combining by jsonobject

--- src/com/zappos/intern/Zappos.java
+++ src/com/zappos/intern/Zappos.java
@@ -1,46 +1,116 @@
 package com.zappos.intern;
-import org.paukov.combinatorics.Factory;
-import org.paukov.combinatorics.Generator;
-import org.paukov.combinatorics.ICombinatoricsVector;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static us.monoid.web.Resty.*;
+import us.monoid.json.JSONArray;
+import us.monoid.json.JSONException;
+import us.monoid.json.JSONObject;
+import us.monoid.web.JSONResource;
+import us.monoid.web.Resty;
+
 
 public class Zappos {
-	private final static int total=147;
+	private final static double total=400.00;
 	private final static int k=3;
 	
-	private ICombinatoricsVector<Integer> closest=null;
-	private int close=0;
-	private int delta=total;
-	
-//	public void Zappos(){
-//		
-//	}
+	private ArrayList<JSONObject> closest=null;
+	private double close=0;
+	private double delta=total;
+
 	
 	public static void main(String args[]){
-		Integer[] arr=new Integer[] { 1,2,50,40,59,69,20,79,440,29,102,440 };
 		Zappos z=new Zappos();
-		z.findClosest(arr);
+		z.test();
+		System.out.println(">>"+z.getClosest().toString());
 	}
 	
-	public void findClosest(Integer[] arr){
-		ICombinatoricsVector<Integer> initialVector = Factory.createVector(arr);
-		int cnt=0;
-	    Generator<Integer> gen = Factory.createSimpleCombinationGenerator(initialVector, k);
-	    for (ICombinatoricsVector<Integer> combination : gen) {
-	    	//cnt++;
-	    	int sum=0;
-	    	for(int i=0;i<k;i++){
-	    		sum=sum+combination.getValue(i);
-	    	}
-	    	int d=Math.abs(total-sum);
-	    	if(d<delta){
-	    		delta=d;
-	    		//cnt++;
-	    		close=sum;
-	    		System.out.println(combination+"--"+sum+"--");
-	    	}
+	public void test(){
+		Resty r = new Resty();
+		try {
+			JSONResource j = r.json("http://api.zappos.com/Search?term=boots&key=52ddafbe3ee659bad97fcce7c53592916a6bfd73");
+			this.iterateOnJSON(j.object());
+		} catch (IOException e) {
+			System.out.println(e.toString());
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void testCloseness(double sum,ArrayList<JSONObject> arr){
+		double d=Math.abs(total-sum);
+    	//System.out.println(combination+"--"+sum+"--"+d);
+    	if(d<delta){
+    		delta=d;
+    		closest=arr;
+    		close=sum;
+    		System.out.println(arr.toString()+"--"+sum+"--");
+    	}
+	}
+	
+	public void iterateOnJSON(JSONObject json){
+		if(json.has("results")){
+			ArrayList<JSONObject> arr=new ArrayList<JSONObject>();
+				try {
+					JSONArray jsona=json.getJSONArray("results");
+					for(int i=0;i<jsona.length();i++){
+						JSONObject product = jsona.getJSONObject(i);
+						arr.add(product);
+					}
+					//System.out.println(arr.toString());
+					this.printCombination(arr, arr.size(), k);
+				} catch (JSONException e) {
+					System.out.println("results not found");
+				}
+		}
+	}
+
+	public void combinationUtil(ArrayList<JSONObject> arr, ArrayList<JSONObject> data, int start, int end, int index, int r)
+	{
+	    // Current combination is ready to be printed, print it
+	    if (index == r)
+	    {
+	    	double sum=0;
+	        for (int j=0; j<r; j++){
+	        	try {
+	        		String price=data.get(j).getString("price");
+	        		price=price.substring(1,price.length());
+	        		sum=sum+Double.parseDouble(price);
+					//System.out.print(price+"|");
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        }	
+	        testCloseness(sum,data);
+	        //System.out.print(sum);
+	        //System.out.println();
+	        return;
 	    }
-	    System.out.println(close);
+	 
+	    
+	    for (int i=start; i<=end && end-i+1 >= r-index; i++)
+	    {	//System.out.println("--"+index);
+	    	if(data.size()<r)
+	    		data.add(index,arr.get(i)) ;
+	    	else
+	    		data.set(index, arr.get(i));
+	        combinationUtil(arr, data, i+1, end, index+1, r);
+	    }
 	}
 	
+	void printCombination(ArrayList<JSONObject> arr, int n, int r)
+	{
+		ArrayList<JSONObject> data=new ArrayList<JSONObject>(3);
+	    combinationUtil(arr, data, 0, n-1, 0, r);
+	}
+	
+	public ArrayList<JSONObject> getClosest(){
+		return closest;
+	}
 	   
 }
